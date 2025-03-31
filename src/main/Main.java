fac838004316d4b04232fe91f7a894b42998967b
XDProjecta <gimosquera@uninorte.edu.co>
2025-03-31 13:36:10 -0500

    viva el cacho!

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,10 @@
-
 package main;
 
-
 public class Main {
+
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("hola mundo cachón!!!");
-=======
-        System.out.println("hola mundo cachona!!!");
->>>>>>> cachona
+        System.out.println("hola mundo cachoooo!!!");
+
     }
-    
-    
-    
+
 }
